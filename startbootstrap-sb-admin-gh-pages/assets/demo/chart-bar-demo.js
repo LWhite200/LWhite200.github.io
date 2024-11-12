ede2d090b4d29c4cfbc395d9a137fc8c7c3fa8a9
LWhite200 <157498802+LWhite200@users.noreply.github.com>
2024-11-12 10:19:52 -0500

    Add files via upload

--- startbootstrap-sb-admin-gh-pages/assets/demo/chart-bar-demo.js
+++ startbootstrap-sb-admin-gh-pages/assets/demo/chart-bar-demo.js
@@ -2,17 +2,44 @@
 Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#292b2c';
 
+//-_-=-=-=-=-=-=-=--=-=-=-=--=-=-=-=--=-=--=-=-=--=-=-=-
+// Declare variables, then pull from the webstie into a json boob, then set these to be those
+var Cute = fetchData(cute);
+var Orange = 100;
+var Fluffy = 100;
+var Sleepy = 100;
+var Silly = 100;
+var Bread = 100;
+
+async function fetchData(tag) {
+  try {
+    const response = await fetch(`https://cataas.com/api/cats?tags=${tag}`); // Use the tag parameter here
+    if (!response.ok) {
+      throw new Error("Error happened!!!");
+    }
+
+    const data = await response.json();    // comes in as a JSON
+    alert(data.length);
+    return data.length;                    // return the number of objects
+  }
+  catch (error) {
+    alert("You screwed up: " + error.message);
+  }
+}
+
+
+
 // Bar Chart Example
 var ctx = document.getElementById("myBarChart");
 var myLineChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["Cute", "Orange", "Fluffy", "Sleepy", "Silly", "bread"],
     datasets: [{
       label: "Revenue",
       backgroundColor: "rgba(2,117,216,1)",
       borderColor: "rgba(2,117,216,1)",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
+      data: [Cute, Orange, Fluffy, Sleepy, Silly, Bread],
     }],
   },
   options: {
@@ -31,7 +58,7 @@ var myLineChart = new Chart(ctx, {
       yAxes: [{
         ticks: {
           min: 0,
-          max: 15000,
+          max: 150,
           maxTicksLimit: 5
         },
         gridLines: {
